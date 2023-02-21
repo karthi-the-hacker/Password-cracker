22cdaa6510b008e05576dccb06052d03cd84aacb
karthikeyan.V <33289300+karthi-the-hacker@users.noreply.github.com>
2023-02-21 10:14:30 +0530

    Update passcrack.py

--- passcrack.py
+++ passcrack.py
@@ -1,29 +1,26 @@
-import hashlib 
-print("**************PASSWORD CRACKER ******************") 
+import hashlib
+print("**************PASSWORD CRACKER ******************")
 
-pass_found = 0         
+pass_found = 0
 
-input_hash = input("Enter the hashed password:") 
+input_hash = input("Enter the hashed password:")
 
-pass_doc = input("\nEnter passwords filename including path(root / home/):") 
+pass_doc = input("\nEnter passwords filename including path(root / home/):")
 
 try:
- pass_file = open(pass_doc, 'r')    
-except: 
- print("Error:") 
- print(pass_doc, "is not found.\nPlease give the path of file correctly.") 
+ pass_file = open(pass_doc, 'r')
+except:
+ print("Error:")
+ print(pass_doc, "is not found.\nPlease give the path of file correctly.")
  quit()
- 
-for word in pass_file: 
- enc_word = word.encode('utf-8') 
- hash_word = hashlib.md5(enc_word.strip()) 
- digest = hash_word.hexdigest() 
- 
- if digest == input_hash: 
-     print("Password found.\nThe password is:", word) 
+
+for word in pass_file:
+ #print(word)
+ enc_word = word.encode('utf-8')
+ hash_word = hashlib.md5(enc_word.strip())
+ digest = hash_word.hexdigest()
+ #print(digest)
+ if digest == input_hash:
+     print("Password found.\nThe password is:", word)
  pass_found = 1
- break
-if not pass_found: 
- print("Password is not found in the", pass_doc, "file") 
- print('\n') 
-print("***************** Thank you **********************") 
+
