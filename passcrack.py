7786bf817e861ae6a880692630ecb3a824f07194
karthikeyan.V <33289300+karthi-the-hacker@users.noreply.github.com>
2023-04-27 10:48:48 +0530

    Update passcrack.py

--- passcrack.py
+++ passcrack.py
@@ -2,18 +2,14 @@ import hashlib
 print("**************PASSWORD CRACKER ******************")
 
 pass_found = 0
-
 input_hash = input("Enter the hashed password:")
-
 pass_doc = input("\nEnter passwords filename including path(root / home/):")
-
 try:
  pass_file = open(pass_doc, 'r')
 except:
  print("Error:")
  print(pass_doc, "is not found.\nPlease give the path of file correctly.")
  quit()
-
 for word in pass_file:
  #print(word)
  enc_word = word.encode('utf-8')
